b8828a5d784605a52db0733a0bf5d7ba819ea388
RamyPoe <coolshifter99@gmail.com>
2024-06-25 19:27:28 -0400

    chore: add circle tests

---
+++ TestCircle.java
@@ -0,0 +1,25 @@
+package lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCircle {
+	
+	private static final double ERR = 0.000001;
+
+	@Test
+	public void testCircleArea() {
+		Point a = new Point(0, 0);
+		Circle circ = new Circle(a, 1);
+		assertEquals(circ.area(), Math.PI, ERR);
+	}
+
+	@Test
+	public void testCircleCircumfrence() {
+		Point a = new Point(0, 0);
+		Circle circ = new Circle(a, 0.5);
+		assertEquals(circ.circumfrence(), Math.PI, ERR);
+	}
+	
+}
